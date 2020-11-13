d468b5d7a061ece57b427914b3ebf68cc0126ed1
EEDiscordiaBot <72722387+EEDiscordiaBot@users.noreply.github.com>
2020-11-12 16:35:24 -0800

    Update Bot.lua

--- Bot.lua
+++ Bot.lua
@@ -1,6 +1,6 @@
 local Discordia = require("discordia")
-local Client = Discordia.Client()
-local DogPics = {
+local client = Discordia.Client()
+local dogPics = {
     {"acting dog", "https://upload.wikimedia.org/wikipedia/commons/4/43/Ace_The_Wonder_Dog.jpg"},
     {"sledding dogs", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/The_dogs_tethered_out_at_The_Grottoes.jpg/800px-The_dogs_tethered_out_at_The_Grottoes.jpg"},
     {"Flickr user nickobec (Nick Cowie), CC BY-SA 2.0\nhttps://creativecommons.org/licenses/by-sa/2.0, via Wikimedia Commons", "https://upload.wikimedia.org/wikipedia/commons/thumb/b/be/Blue_merle_Border_Collie.jpg/800px-Blue_merle_Border_Collie.jpg"},
@@ -13,37 +13,37 @@ local DogPics = {
     {"snow dog", "https://upload.wikimedia.org/wikipedia/commons/thumb/3/32/Afra_011.jpg/800px-Afra_011.jpg"}
 }
 
-Client:on("messageCreate", function(Message)
+client:on("messageCreate", function(message)
 
-    if Message.channel.type == 0 or Message.channel.type == 4 then
+    if message.channel.type == 0 or message.channel.type == 4 then
 
-        if Message.author.bot == false and Message.content == "random dog" and Message.guild:getMember("YourBotIDHere"):hasPermission(Message.channel, "YourBotIDHere") and Message.guild:getMember("YourBotIDHere"):hasPermission(Message.channel, "sendMessages") then
+        if message.author.bot == false and message.content == "random dog" and message.guild.me:getPermissions(message.channel):has("sendMessages", "attatchFiles") then
 
-            local Decider = math.random(#DogPics)
+            local decider = math.random(#dogPics)
 
-            Message.channel:send {
+            message.channel:send {
                 embed = {
-                    description = DogPics[Decider][1],
+                    description = dogPics[decider][1],
                     image = {
-                        url = DogPics[Decider][2]
+                        url = dogPics[decider][2]
                     }
                 }
             }
-        elseif Message.content == "random dog" then
-            Message.author:send("Sorry, I don't have permission to send a dog image in that channel.")
+        elseif message.content == "random dog" then
+            message.author:send("Sorry, I don't have permission to send a dog image in that channel.")
         end
 
-    elseif Message.channel.type == 3 or Message.channel.type == 1 and Message.author.bot == false and Message.content == "random dog" then
-        local Decider = math.random(#DogPics)
-        Message.channel:send {
+    elseif message.channel.type == 3 or message.channel.type == 1 and message.author.bot == false and message.content == "random dog" then
+        local decider = math.random(#dogPics)
+        message.channel:send {
             embed = {
-                description = DogPics[Decider][1],
+                description = dogPics[decider][1],
                 image = {
-                    url = DogPics[Decider][2]
+                    url = dogPics[decider][2]
                 }
             }
         }
     end
 end)
 
-Client:run("Bot YourBotTokenHere")
+client:run("Bot YourBotTokenHere")
